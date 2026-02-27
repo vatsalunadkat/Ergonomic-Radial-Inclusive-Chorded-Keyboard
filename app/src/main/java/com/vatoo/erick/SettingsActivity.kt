5352c9b9f31c8657696efaec9cfc6dbdab6cca81
Vatsal Unadkat <31386890+vatsalunadkat@users.noreply.github.com>
2026-02-27 20:03:43 +0530

    Drop MAIN intent; make SettingsActivity Activity
    Remove the android.intent.action.MAIN entry from the input method intent-filter in AndroidManifest.xml (IME services should not declare the MAIN launcher action). Change SettingsActivity to extend android.app.Activity instead of AppCompatActivity and update imports accordingly.

--- app/src/main/java/com/vatoo/erick/SettingsActivity.kt
+++ app/src/main/java/com/vatoo/erick/SettingsActivity.kt
@@ -1,9 +1,9 @@
 package com.vatoo.erick
 
+import android.app.Activity
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
