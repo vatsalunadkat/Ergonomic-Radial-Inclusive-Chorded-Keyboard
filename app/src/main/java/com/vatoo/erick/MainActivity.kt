cbbefd6be08bf09b5f702dfd461d8a41c0a3b475
aditursynn <91518877+aditursynn@users.noreply.github.com>
2026-03-04 14:16:26 +0100

    committing before checking out Nazgul's branch

--- app/src/main/java/com/vatoo/erick/MainActivity.kt
+++ app/src/main/java/com/vatoo/erick/MainActivity.kt
@@ -1,16 +1,17 @@
 package com.vatoo.erick
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.asImageBitmap
 import com.vatoo.erick.ui.theme.ERICKTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +21,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             ERICKTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+
+                    val spriteSheet = remember {
+                        BitmapFactory.decodeResource(resources, R.drawable.potions_full_corked)
+                    }
+
+                    val tileWidth = 16
+                    val tileHeight = 24
+
+                    val firstPotion = remember {
+                        android.graphics.Bitmap.createBitmap(spriteSheet, 0, 0, tileWidth, tileHeight)
+                    }
+
+                    Image(
+                        bitmap = firstPotion.asImageBitmap(),
+                        contentDescription = "Potion",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ERICKTheme {
-        Greeting("Android")
     }
 }
